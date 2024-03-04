7f412a84fd17c18bea037f1fe4b420e0d351f310
Kacper Gadek <kacper.gadek2@wp.pl>
2024-03-04 10:49:38 +0100

    Revert "dodanie hej"
    This reverts commit a702ed2df5185776745a465129fda9aedeb35097.

--- src/main/java/com/example/javafx_gra/HelloController.java
+++ src/main/java/com/example/javafx_gra/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!hejjjjjj");
+        welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
